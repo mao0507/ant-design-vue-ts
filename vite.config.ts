65c60e7a6a4cc8022643268e9cb3df9b6f7ec303
mao <maomao@aufouce.com>
2023-10-19 15:15:37 +0800

    feat:設定套件使用

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,28 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// 自動引入 Icon
+import Components from 'unplugin-vue-components/vite'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    Components({
+      resolvers: [
+        IconsResolver({
+          prefix: false,
+          enabledCollections: ['ant-design'],
+        }),
+      ],
+    }),
+    Icons({}),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
