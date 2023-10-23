3c518a34ff0349d25b1927515e0bf208d08d9cc9
mao <maomao@aufouce.com>
2023-10-23 16:43:36 +0800

    feat:拆出Card組件

---
+++ src/layout/CardView.vue
@@ -0,0 +1,36 @@
+<template>
+<a-layout-content
+  :style="{ 
+    margin: '24px 16px 12px 16px', 
+    padding: '24px',
+    background: '#fff',
+    minHeight: minH,
+    maxHeight: maxH
+  }"
+>
+  <slot></slot>
+</a-layout-content>
+</template>
+
+<script lang='ts'>
+export default {
+  props: {
+    min: {
+      type: Number,
+      default:280
+    },
+    max: {
+      type: Number,
+      default:800
+    }
+  },
+  setup(props) {
+    const minH = props.min + 'px'
+    const maxH = props.max + 'px'
+    return {
+      minH,
+      maxH
+    }
+  },
+};
+</script>
