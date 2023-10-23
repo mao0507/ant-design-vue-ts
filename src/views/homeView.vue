138496db58e8bf9bbba091cd85cb1973e590e56f
mao <maomao@aufouce.com>
2023-10-23 16:43:00 +0800

    feat:建立起始頁面

---
+++ src/views/homeView.vue
@@ -0,0 +1,22 @@
+<template>
+<CardView>
+  <h3>
+    Hello world
+  </h3>
+</CardView> 
+</template>
+
+<script lang='ts'>
+import CardView from '../layout/CardView.vue';
+
+export default {
+
+  components: {
+    CardView
+  },
+
+  setup() {
+    return {};
+  },
+};
+</script>
