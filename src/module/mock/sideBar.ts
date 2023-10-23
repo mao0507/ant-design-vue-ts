3c7bf59270ea76ad962bc01089bbc132909338b0
mao <maomao@aufouce.com>
2023-10-23 16:41:11 +0800

    feat:調整module

---
+++ src/module/mock/sideBar.ts
@@ -0,0 +1,24 @@
+//sideBar 的 Module 需要依照個專案搭配的後端自行調整
+
+export interface sideBar {
+  featuresId: string
+  code: string
+  name: string
+  url: string
+  icon: string
+  parentId: any
+  sort: number
+  subMenu: SubMenu[]
+}
+
+export interface SubMenu {
+  featuresId: string
+  code: string
+  name: string
+  url: string
+  featuresType?: number
+  icon: string
+  parentId: string
+  sort: number
+  subMenu: SubMenu[]
+}
