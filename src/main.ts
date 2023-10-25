45b75af1344c0b9529a102b09dfd1c920568f623
mao <maomao@aufouce.com>
2023-10-25 11:08:34 +0800

    feat:引入樣式包

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { createPinia } from 'pinia'
 // 註冊 ant Design
 import 'ant-design-vue/dist/reset.css'
 import Antd from 'ant-design-vue'
+// 引入樣式
+import '../src/assets/scss/app.scss'
 
 // 註冊 chart js
 import Chart from 'chart.js/auto'
