e147ae319f67a617bad51fcc210dcf77d709c0c3
mao <maomao@aufouce.com>
2023-10-25 11:08:25 +0800

    feat:更換使用組件，替換成card，調整props判斷

--- src/layout/CardView.vue
+++ src/layout/CardView.vue
@@ -1,27 +1,40 @@
 <template>
-<a-layout-content
+<a-card
+  :bordered="false" 
+  :title="props.title"
   :style="{ 
-    'margin-left':props.marginLeft + 'px',
-    'margin-top':props.marginTop + 'px',
-    'margin-right':props.marginRight + 'px',
-    'margin-bottom':props.marginBottom + 'px' ,
-    'padding-left':props.paddingLeft + 'px',
-    'padding-top':props.paddingTop + 'px',
-    'padding-right':props.paddingRight + 'px',
-    'padding-bottom':props.paddingBottom + 'px' ,
-    background: '#fff',
-    minHeight: props.minHeight + 'px',
-    maxHeight: props.maxHeight + 'px'
-  }"
->
+      'margin-left':props.marginLeft != 0 ? props.marginLeft + 'px' : '',
+      'margin-top':props.marginTop != 0 ? props.marginTop + 'px' : '',
+      'margin-right':props.marginRight != 0 ? props.marginRight + 'px' : '',
+      'margin-bottom':props.marginBottom != 0 ? props.marginBottom + 'px' : '',
+      'padding-left':props.paddingLeft != 0 ? props.paddingLeft + 'px' : '',
+      'padding-top':props.paddingTop != 0 ? props.paddingTop + 'px' : '',
+      'padding-right':props.paddingRight != 0 ? props.paddingRight + 'px' : '',
+      'padding-bottom':props.paddingBottom != 0 ? props.paddingBottom + 'px' : '',
+      'min-Height': props.minHeight != 0 ? props.minHeight + 'px' : '',
+      'max-Height': props.maxHeight != 0 ? props.maxHeight + 'px' : '',
+      'min-width': props.minWidth != 0 ? props.minWidth + 'px' : '',
+      'max-width': props.maxWidth != 0 ? props.maxWidth + 'px' : '',
+    }"
+  >
   <!-- 內嵌項目 -->
   <slot></slot>
-</a-layout-content>
+</a-card>
 </template>
 
 <script lang='ts'>
 export default {
   props: {
+    title: {
+      type:String
+    },
+    //寬度相關
+    minWidth: {
+      type: Number,
+    },
+    maxWidth: {
+      type: Number,
+    },
     //高度相關
     minHeight: {
       type: Number,
@@ -29,7 +42,6 @@ export default {
     },
     maxHeight: {
       type: Number,
-      default:800
     },
     //內距處理
     paddingLeft: {
