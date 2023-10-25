82f8c4b8ef1d59e57a648a2e0fe18bc5b2ef9a12
mao <maomao@aufouce.com>
2023-10-25 11:27:09 +0800

    feat:調整dashBard

--- src/views/dashBoardView.vue
+++ src/views/dashBoardView.vue
@@ -1,43 +1,38 @@
 <template>
-<a-row>
-  <a-col :span="12">
-    <CardView 
+  <a-row>
+    <a-col :span="12">
+      <!-- <CardView 
       :maxHeight="300"
-      :paddingBottom="30"
+      :paddingBottom="50"
     >
     <h3 style="font-weight: bold;">
       在線人數 (線圖)
     </h3>
     <canvas ref="lineChartEl"></canvas>
-  </CardView> 
-  </a-col>
-  <a-col :span="12">
-    <CardView 
-      :maxHeight="300" 
-      :paddingBottom="30"
-    >
-    <h3>
-      在線人數 (長條圖)
-    </h3>
-    <canvas ref="barChartEl"></canvas>
-  </CardView>
-  </a-col>
-</a-row>
-  
-  
+  </CardView>  -->
 
-<CardView>
-    <h3>
-      DashBoard Page
-    </h3>
-</CardView>
- 
+      <CardView :title="'DashBoard'" :paddingLeft="10" :paddingRight="10" :paddingBottom="10" :paddingTop="10">
+        <canvas ref="lineChartEl"></canvas>
+      </CardView>
+    </a-col>
+    <a-col :span="12">
+      <CardView :title="'在線人數'" :paddingLeft="10" :paddingRight="10" :paddingBottom="10" :paddingTop="10">
+        <canvas ref="barChartEl"></canvas>
+      </CardView>
+    </a-col>
+  </a-row>
+
+
+
+  <CardView :title="'DashBoard'">
+
+  </CardView>
 </template>
 
 <script lang='ts'>
 import CardView from '../layout/CardView.vue';
-import { onMounted, ref  } from 'vue' 
-import { useLineChart,useBarChart } from '../composable/useChart'
+import { onMounted, ref } from 'vue'
+import { useLineChart, useBarChart } from '../composable/useChart'
 export default {
   components: {
     CardView
@@ -47,10 +42,10 @@ export default {
     const barChartEl = ref(null)
     const labels = ['10/16', '10/17', '10/18', '10/19', '10/20', '10/21'];
     const data = [{
-        label: '在線人數數量',
-        data: [150, 105, 155, 200, 180, 360],
-        color: '#7C3AED',
-      },
+      label: '在線人數數量',
+      data: [150, 105, 155, 200, 180, 360],
+      color: '#7C3AED',
+    },
     ]
 
     const lineScales = {
@@ -83,8 +78,8 @@ export default {
 
     onMounted(() => {
       useLineChart(lineChartEl, labels, data, lineScales);
-      useBarChart(barChartEl,labels,data,barScales)
-    }) 
+      useBarChart(barChartEl, labels, data, barScales)
+    })
 
     return {
       lineChartEl,
