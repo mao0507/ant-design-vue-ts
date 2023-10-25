bcf5374d95f148df0a891d55be6df3eac06f7dd3
mao <maomao@aufouce.com>
2023-10-25 11:37:07 +0800

    feat:包裝card卡片組件

--- src/layout/CardView.vue
+++ src/layout/CardView.vue
@@ -1,32 +1,28 @@
 <template>
-<a-card
-  :bordered="false" 
-  :title="props.title"
-  :style="{ 
-      'margin-left':props.marginLeft != 0 ? props.marginLeft + 'px' : '',
-      'margin-top':props.marginTop != 0 ? props.marginTop + 'px' : '',
-      'margin-right':props.marginRight != 0 ? props.marginRight + 'px' : '',
-      'margin-bottom':props.marginBottom != 0 ? props.marginBottom + 'px' : '',
-      'padding-left':props.paddingLeft != 0 ? props.paddingLeft + 'px' : '',
-      'padding-top':props.paddingTop != 0 ? props.paddingTop + 'px' : '',
-      'padding-right':props.paddingRight != 0 ? props.paddingRight + 'px' : '',
-      'padding-bottom':props.paddingBottom != 0 ? props.paddingBottom + 'px' : '',
-      'min-Height': props.minHeight != 0 ? props.minHeight + 'px' : '',
-      'max-Height': props.maxHeight != 0 ? props.maxHeight + 'px' : '',
-      'min-width': props.minWidth != 0 ? props.minWidth + 'px' : '',
-      'max-width': props.maxWidth != 0 ? props.maxWidth + 'px' : '',
-    }"
-  >
-  <!-- 內嵌項目 -->
-  <slot></slot>
-</a-card>
+  <a-card :bordered="false" :title="props.title" :style="{
+    'margin-left': props.marginLeft != 0 ? props.marginLeft + 'px' : '',
+    'margin-top': props.marginTop != 0 ? props.marginTop + 'px' : '',
+    'margin-right': props.marginRight != 0 ? props.marginRight + 'px' : '',
+    'margin-bottom': props.marginBottom != 0 ? props.marginBottom + 'px' : '',
+    'padding-left': props.paddingLeft != 0 ? props.paddingLeft + 'px' : '',
+    'padding-top': props.paddingTop != 0 ? props.paddingTop + 'px' : '',
+    'padding-right': props.paddingRight != 0 ? props.paddingRight + 'px' : '',
+    'padding-bottom': props.paddingBottom != 0 ? props.paddingBottom + 'px' : '',
+    'min-Height': props.minHeight != 0 ? props.minHeight + 'px' : '',
+    'max-Height': props.maxHeight != 0 ? props.maxHeight + 'px' : '',
+    'min-width': props.minWidth != 0 ? props.minWidth + 'px' : '',
+    'max-width': props.maxWidth != 0 ? props.maxWidth + 'px' : '',
+  }">
+    <!-- 內嵌項目 -->
+    <slot></slot>
+  </a-card>
 </template>
 
 <script lang='ts'>
 export default {
   props: {
     title: {
-      type:String
+      type: String
     },
     //寬度相關
     minWidth: {
@@ -38,7 +34,7 @@ export default {
     //高度相關
     minHeight: {
       type: Number,
-      default:280
+      default: 280
     },
     maxHeight: {
       type: Number,
@@ -46,36 +42,36 @@ export default {
     //內距處理
     paddingLeft: {
       type: Number,
-      default:24
+      default: 24
     },
     paddingRight: {
       type: Number,
-      default:24
+      default: 24
     },
     paddingTop: {
       type: Number,
-      default:24
+      default: 24
     },
     paddingBottom: {
       type: Number,
-      default:24
+      default: 24
     },
     //外距
     marginTop: {
       type: Number,
-      default:24
+      default: 24
     },
     marginLeft: {
       type: Number,
-      default:16
+      default: 16
     },
     marginRight: {
       type: Number,
-      default:16
+      default: 16
     },
     marginBottom: {
       type: Number,
-      default:16
+      default: 16
     },
   },
   setup(props) {
