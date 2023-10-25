35fee72a2f8cb4acbfac533e85419802530f8cbb
mao <maomao@aufouce.com>
2023-10-25 14:03:02 +0800

    feat:調整eslint設定

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,35 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
+    'airbnb-base',
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: 'latest',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'quotes': [2, 'single']
+    quotes: [2, 'single'],
   },
-}
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@', './src'],
+        ],
+        extensions: ['.js', '.vue'],
+      },
+    },
+    'import/core-modules': [
+      'vite',
+      '@vitejs/plugin-vue',
+    ],
+  },
+};
