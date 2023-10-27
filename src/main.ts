9ccf6db31beb86cd523a8718d452d186cbdebcbd
mao <maomao@aufouce.com>
2023-10-27 16:53:17 +0800

    feat:註冊i18n

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,22 @@
 /* eslint-disable */
-
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 // 註冊 ant Design
 import 'ant-design-vue/dist/reset.css';
 import Antd from 'ant-design-vue';
 // 引入樣式
-import '../src/assets/scss/app.scss';
-
-// 註冊 chart js
-import Chart from 'chart.js/auto';
-
-import App from './App.vue';
-import router from './router';
+import '@/assets/scss/app.scss';
+// i18n;
+import { i18n } from '@/plugins/i18n';
+// router
+import router from '@/router';
+import App from '@/App.vue';
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 app.use(Antd);
+app.use(i18n);
 
 app.mount('#app');
