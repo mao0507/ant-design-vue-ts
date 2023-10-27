ae5ca391c7ab9e643a1a5381f95172723c81ee2d
mao <maomao@aufouce.com>
2023-10-27 16:52:38 +0800

    feat:加入i18n功能，語系切換功能

--- src/layout/HeaderView.vue
+++ src/layout/HeaderView.vue
@@ -2,10 +2,7 @@
   <a-layout-header class="headerNav">
     <!-- sideBar switch Button -->
     <div>
-      <ant-menu-outlined
-        class="trigger"
-        @click="switchDisplay()"
-      />
+      <ant-menu-outlined class="trigger" @click="switchDisplay()" />
     </div>
 
     <div class="headerButtonList">
@@ -14,23 +11,18 @@
       <a-dropdown :trigger="['click']" arrow>
         <template #overlay>
           <a-menu>
-            <a-menu-item key="1">
-              <UserOutlined />
-              1st menu item
-            </a-menu-item>
-            <a-menu-item key="2">
-              <UserOutlined />
-              2nd menu item
-            </a-menu-item>
-            <a-menu-item key="3">
-              <UserOutlined />
-              3rd item
+            <a-menu-item
+              v-for="item in langsList"
+              :key="item.value"
+              @click="changeLang(item.value)"
+            >
+              {{ item.text }}
             </a-menu-item>
           </a-menu>
         </template>
-        <a-button class="headerButton" style="margin-right: 10px;">
-          <ant-message-filled/>
-          <span >Zh-tw</span>
+        <a-button class="headerButton" style="margin-right: 10px">
+          <ant-message-filled />
+          <span>{{ nowLang }}</span>
         </a-button>
       </a-dropdown>
 
@@ -39,20 +31,18 @@
       <a-dropdown :trigger="['click']" arrow>
         <template #overlay>
           <a-menu>
-            <a-menu-item key="1" >
+            <a-menu-item key="1">
               <div class="menuItem">
-                <ant-setting-outlined/>
-                <span>更改密碼</span>
+                <ant-setting-outlined />
+                <span>{{ $t('P00009') }}</span>
               </div>
             </a-menu-item>
-            <a-menu-item key="2" >
+            <a-menu-item key="2">
               <div class="menuItem">
                 <ant-key-outlined />
-                <span>登出</span>
+                <span>{{ $t('P00010') }}</span>
               </div>
-
             </a-menu-item>
-
           </a-menu>
         </template>
         <a-button class="headerButton">
@@ -60,56 +50,58 @@
           <span>Admin</span>
         </a-button>
       </a-dropdown>
-
     </div>
-
   </a-layout-header>
 </template>
 
-<script lang='ts'>
+<script lang="ts">
 import { computed } from 'vue';
-import { useSideBarStore } from '../stores/sideBarStore';
+import { langsList, getNowLang, changeLang } from '@/composable/useLangs';
+import { useSideBarStore } from '@/stores/sideBarStore';
 
 export default {
   setup() {
     const store = useSideBarStore();
     const display = computed(() => store.display);
     const switchDisplay = () => store.switchDisplay();
+    const nowLang = getNowLang();
 
     return {
       display,
       switchDisplay,
+      langsList,
+      nowLang,
+      changeLang,
     };
   },
 };
 </script>
 <style lang="sass" scoped>
-  .headerNav
-    background: #fff !important
-    padding-inline: 32px !important
-    display: flex
-    align-items: center
-    justify-content: space-between
+.headerNav
+  background: #fff !important
+  padding-inline: 32px !important
+  display: flex
+  align-items: center
+  justify-content: space-between
 
-  .headerButtonList
-    display: flex
+.headerButtonList
+  display: flex
 
-  .headerButton
-    display: flex
-    align-items: center
-    justify-content: space-between
-    > svg
-      margin-right: 5px
+.headerButton
+  display: flex
+  align-items: center
+  justify-content: space-between
+  > svg
+    margin-right: 5px
 
-  .menuItem
-    display: flex
-    align-items: center
-    > svg
-      margin-right: 5px
-    > span
-      margin: 0px 3px
+.menuItem
+  display: flex
+  align-items: center
+  > svg
+    margin-right: 5px
+  > span
+    margin: 0px 3px
 
-  .trigger:hover
-    cursor: pointer
-
+.trigger:hover
+  cursor: pointer
 </style>
