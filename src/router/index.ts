ff9e1451edeb666b03accd23e2d81eb2f8255935
mao <maomao@aufouce.com>
2023-11-01 14:16:34 +0800

    feat:調整router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,35 +1,49 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import LayoutView from '../layout/LayoutView.vue';
-// DEV test Page
-import HomeView from '../views/homeView.vue';
+import { getStorage, setStorage } from '@/composable/useStorage';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: LayoutView,
-      children: [
-        {
-          path: '/',
-          name: 'home',
-          component: HomeView,
-          // component: () => import('../views/homeView.vue')
-        },
-        {
-          path: '/dashBoard',
-          name: 'dashBoard',
-          component: () => import('../views/dashBoardView.vue'),
-        },
-      ],
+      name: 'layout',
+      component: () => import('@/layout/LayoutView.vue'),
+      children: [],
     },
     {
       path: '/login',
       name: 'login',
-      component: () => import('../views/LoginView.vue'),
+      component: () => import('@/views/LoginView.vue'),
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      redirect: '/',
     },
   ],
+});
+
+// 建立路由守衛，透過路由守衛來檢查 token
+router.beforeEach((to, from, next) => {
+  // 檢查是否在登入頁面
+  if (to.name !== 'login') {
+    // 嘗試抓取token
+    const token = getStorage('token');
+    if (token === null || token === '') {
+      // 沒有token的話，重新導向login
+      next({ name: 'login', path: '/login' });
+    } else {
+      next();
+    }
+  } else next();
+});
+
+// 紀錄當前頁面，防止重新刷新後丟失當前頁面
+router.afterEach((to, from) => {
+  console.log('to', to);
+  console.log('from', from);
+  if (to.path !== '/' || from.path !== '/') {
+    setStorage('nowPath', to.path);
+  }
 });
 
 export default router;
