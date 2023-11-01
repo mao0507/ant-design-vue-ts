b7a118d2bd59ad6ee7928b9120951801636b3853
mao <maomao@aufouce.com>
2023-11-01 14:13:49 +0800

    feat:增加註解

--- src/composable/useLogin.ts
+++ src/composable/useLogin.ts
@@ -1,6 +1,11 @@
 import type { loginInfo } from '@/module/loginModule';
 import { loginAPI } from '@/service/auth';
 
+/**
+ * 登入
+ * @param  { loginInfo } data 登入所需資訊
+ * @returns { string } 回傳登入狀態
+ */
 export function login(data: loginInfo) {
   const respond = loginAPI(data);
   return respond;
